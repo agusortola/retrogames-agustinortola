567475e19a82d2d985d0904d66ccccc835847ae2
agusortola <63934179+agusortola@users.noreply.github.com>
2021-05-19 20:20:12 -0300

    query one item test

--- src/components/Product/ItemList.js
+++ src/components/Product/ItemList.js
@@ -1,33 +1,12 @@
 import { Item }  from './Item';
 import './ItemList.css';
-import {useEffect, useState} from 'react';
 import { Grid } from '@material-ui/core';
-import { getFireStore } from '../../firebase';
-
 
 
 const ItemList = (props) => {
-
-    const [items, setItems] = useState([])
-    const [isEmptyCategory, setIsEmptyCategory] = useState(false)
-    
-    useEffect(
-      () => {
-        const db = getFireStore()
-        const itemColection = db.collection('items')
-        itemColection.get().then(
-          (querySnapshot) => {
-           if (querySnapshot.size === 0) {
-              setIsEmptyCategory(true)
-           }
-          setItems(querySnapshot.docs.map((doc) => doc.data()))
-          }).catch(
-            (error) => console.error("Firestore error:", error)
-          )
-      }, [])
-
+  console.log(props.items)
     return (
-    
+      
           <Grid
             className="itemlist"
             container
@@ -36,14 +15,13 @@ const ItemList = (props) => {
             alignItems="center"
             spacing={10}
           >
-            {items.filter(i => props.category == null || i.console == props.category).map((item) => (
+            {props.items.filter(i => props.category == null || i.categoryId == props.category).map((item) => (
               <Grid item xs={4}>
                 <Item 
                   img={item.img} 
                   title={item.title} 
                   price={item.price} 
                   console={item.categoryId} 
-                  
                   id= {item.id}
                 />
               </Grid>
--- src/components/Product/ItemListContainer.js
+++ src/components/Product/ItemListContainer.js
@@ -1,15 +1,35 @@
 import ItemList from "./ItemList";
 import {useParams} from 'react-router-dom';
 import "./itemListContainer.css";
-
+import {useEffect, useState} from 'react';
+import { Grid } from '@material-ui/core';
+import { getFireStore } from '../../firebase';
 
 export const  ItemListContainer = (props) => {
   
   const { categoryId } = useParams();
 
+  const [items, setItems] = useState([])
+  const [isEmptyCategory, setIsEmptyCategory] = useState(false)
+  
+  useEffect(
+    () => {
+      const db = getFireStore()
+      const itemColection = db.collection('items')
+      itemColection.get().then(
+        (querySnapshot) => {
+         if (querySnapshot.size === 0) {
+            setIsEmptyCategory(true)
+         }
+        setItems(querySnapshot.docs.map((doc) => doc.data()))
+        }).catch(
+          (error) => console.error("Firestore error:", error)
+        )
+    }, [])
+
   return (
     <div className="itemListContainer">
-      <ItemList category={categoryId} />
+      <ItemList category={categoryId} items={items}/>
     </div>
   );
 };
