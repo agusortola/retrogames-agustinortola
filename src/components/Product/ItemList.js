2ddf1d90f3e7fdb843e154b966fe3621e78579dd
agusortola <63934179+agusortola@users.noreply.github.com>
2021-05-19 10:13:05 -0300

    añadí Firebase

--- src/components/Product/ItemList.js
+++ src/components/Product/ItemList.js
@@ -1,84 +1,30 @@
 import { Item }  from './Item';
 import './ItemList.css';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import { Grid } from '@material-ui/core';
+import { getFireStore } from '../../firebase';
 
 
 
 const ItemList = (props) => {
 
     const [items, setItems] = useState([])
-    const db = [
-        {
-          img: 'https://i.imgur.com/4l2kSvN.jpg',
-          title: 'Monkey Island I',
-          price: 9.90,
-          console: 'nintendo',
-          categoryID: 1,
-          description: 'Aventura gráfica - 1990.',
-          detailedDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit massa sociosqu phasellus aptent, augue accumsan sagittis velit elementum aliquam senectus risus felis. Taciti mauris turpis venenatis auctor eleifend enim varius lobortis magna diam, aliquet vulputate senectus gravida montes molestie felis pretium quis massa, praesent himenaeos ante ligula platea faucibus dapibus tempor integer. Diam taciti pharetra ultricies class pretium rutrum bibendum vulputate aliquam platea, tortor iaculis non hendrerit sed sociis senectus pellentesque turpis nunc, condimentum ad eu dui congue tempor commodo faucibus ornare.',
-          id: 1
-        },
-        {
-          img: 'https://i.imgur.com/Qx6xnVG.jpg',
-          title: 'Ristar',
-          price: 7.30,
-          console: 'sega',
-          categoryID: 2,
-          description: 'Plataforma - 1995'  ,
-          detailedDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit massa sociosqu phasellus aptent, augue accumsan sagittis velit elementum aliquam senectus risus felis. Taciti mauris turpis venenatis auctor eleifend enim varius lobortis magna diam, aliquet vulputate senectus gravida montes molestie felis pretium quis massa, praesent himenaeos ante ligula platea faucibus dapibus tempor integer. Diam taciti pharetra ultricies class pretium rutrum bibendum vulputate aliquam platea, tortor iaculis non hendrerit sed sociis senectus pellentesque turpis nunc, condimentum ad eu dui congue tempor commodo faucibus ornare.',
-          id:2
-        },
-        {
-          img: 'https://i.imgur.com/FRQhWjZ.jpg',
-          title: 'Road Rash',
-          price: 12.60,
-          console: 'gameboy',
-          categoryID: 3,
-          description: 'Carreras - 1991',
-          detailedDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit massa sociosqu phasellus aptent, augue accumsan sagittis velit elementum aliquam senectus risus felis. Taciti mauris turpis venenatis auctor eleifend enim varius lobortis magna diam, aliquet vulputate senectus gravida montes molestie felis pretium quis massa, praesent himenaeos ante ligula platea faucibus dapibus tempor integer. Diam taciti pharetra ultricies class pretium rutrum bibendum vulputate aliquam platea, tortor iaculis non hendrerit sed sociis senectus pellentesque turpis nunc, condimentum ad eu dui congue tempor commodo faucibus ornare.',
-          id:3
-        },
-        {
-          img: 'https://i.imgur.com/4l2kSvN.jpg',
-          title: 'Monkey Island I',
-          price: 9.90,
-          console: 'nintendo',
-          categoryID: 1,
-          description: 'Aventura gráfica - 1990.',
-          detailedDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit massa sociosqu phasellus aptent, augue accumsan sagittis velit elementum aliquam senectus risus felis. Taciti mauris turpis venenatis auctor eleifend enim varius lobortis magna diam, aliquet vulputate senectus gravida montes molestie felis pretium quis massa, praesent himenaeos ante ligula platea faucibus dapibus tempor integer. Diam taciti pharetra ultricies class pretium rutrum bibendum vulputate aliquam platea, tortor iaculis non hendrerit sed sociis senectus pellentesque turpis nunc, condimentum ad eu dui congue tempor commodo faucibus ornare.',
-          id: 1
-        },
-        {
-          img: 'https://i.imgur.com/Qx6xnVG.jpg',
-          title: 'Ristar',
-          price: 7.30,
-          console: 'sega',
-          categoryID: 2,
-          description: 'Plataforma - 1995'  ,
-          detailedDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit massa sociosqu phasellus aptent, augue accumsan sagittis velit elementum aliquam senectus risus felis. Taciti mauris turpis venenatis auctor eleifend enim varius lobortis magna diam, aliquet vulputate senectus gravida montes molestie felis pretium quis massa, praesent himenaeos ante ligula platea faucibus dapibus tempor integer. Diam taciti pharetra ultricies class pretium rutrum bibendum vulputate aliquam platea, tortor iaculis non hendrerit sed sociis senectus pellentesque turpis nunc, condimentum ad eu dui congue tempor commodo faucibus ornare.',
-          id:2
-        },
-        {
-          img: 'https://i.imgur.com/FRQhWjZ.jpg',
-          title: 'Road Rash',
-          price: 12.60,
-          console: 'gameboy',
-          categoryID: 3,
-          description: 'Carreras - 1991',
-          detailedDescription: 'Lorem ipsum dolor sit amet consectetur adipiscing elit massa sociosqu phasellus aptent, augue accumsan sagittis velit elementum aliquam senectus risus felis. Taciti mauris turpis venenatis auctor eleifend enim varius lobortis magna diam, aliquet vulputate senectus gravida montes molestie felis pretium quis massa, praesent himenaeos ante ligula platea faucibus dapibus tempor integer. Diam taciti pharetra ultricies class pretium rutrum bibendum vulputate aliquam platea, tortor iaculis non hendrerit sed sociis senectus pellentesque turpis nunc, condimentum ad eu dui congue tempor commodo faucibus ornare.',
-          id:3
-        }
-      ]
-
-      const getItems = (items) => {
-        return new Promise((res, rej) => {
-          setTimeout(()=> {
-            return res (setItems(items))
-          }, 1)
-        })
-      }
-      getItems(db)
+    const [isEmptyCategory, setIsEmptyCategory] = useState(false)
+    
+    useEffect(
+      () => {
+        const db = getFireStore()
+        const itemColection = db.collection('items')
+        itemColection.get().then(
+          (querySnapshot) => {
+           if (querySnapshot.size === 0) {
+              setIsEmptyCategory(true)
+           }
+          setItems(querySnapshot.docs.map((doc) => doc.data()))
+          }).catch(
+            (error) => console.error("Firestore error:", error)
+          )
+      }, [])
 
     return (
     
@@ -96,7 +42,7 @@ const ItemList = (props) => {
                   img={item.img} 
                   title={item.title} 
                   price={item.price} 
-                  console={item.console} 
+                  console={item.categoryId} 
                   
                   id= {item.id}
                 />
---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyANe-xwXBFkQKoQdKnmodfHifwGCq310Ek",
+    authDomain: "retro-games-store.firebaseapp.com",
+    projectId: "retro-games-store",
+    storageBucket: "retro-games-store.appspot.com",
+    messagingSenderId: "182093145817",
+    appId: "1:182093145817:web:53cd04d7581c08b1d740c8",
+    measurementId: "G-ZQW0M639TP"
+ });
+
+export const getFirebase = () => app
+export const getFireStore = () => firebase.firestore(app)
